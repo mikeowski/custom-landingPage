50ab79fa8f4786261d9caf1ee7fe39f06abdf997
Burak Mike <burakmahyk@gmail.com>
2022-09-01 03:57:10 +0300

    feature: changed font family to Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+    },
     extend: {},
   },
   plugins: [],
