14f8c99f59361f53748bbd35abe73caf15fe388e
Burak Mike <burakmahyk@gmail.com>
2022-09-01 03:56:51 +0300

    feature: added new image domains for next/iimage

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['images.unsplash.com', 'lirp.cdn-website.com'],
+  },
 }
 
 module.exports = nextConfig
